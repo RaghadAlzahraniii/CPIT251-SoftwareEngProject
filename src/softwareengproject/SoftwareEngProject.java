2a61467f35414b9e9c78468042c3f666a18ae74e
Atheer_Alghamdi <athwryalghamdy@gmail.com>
2023-11-28 22:00:15 +0300

    edit on code

--- src/softwareengproject/SoftwareEngProject.java
+++ src/softwareengproject/SoftwareEngProject.java
@@ -12,16 +12,19 @@ public class SoftwareEngProject {
     public static void main(String[] args) {
         Coach coach = new Coach("Coach123"); // Replace "Coach123" with actual coach ID
         Trainee trainee = new Trainee();
-
+       Scanner scanner = new Scanner(System.in);
         // Adding a course
-        coach.addCourse("Java Programming", "CS101", 49.99);
+        String name=scanner.next();
+        String courseId=scanner.next();
+        double price=scanner.nextDouble();
+        coach.addCourse("Keto Diet", "KO100", 150.0);
 
         // Displaying courses
         ArrayList<Course> availableCourses = coach.getCourseList();
         trainee.viewCourses(availableCourses);
 
         // Trainee selecting a course
-        Scanner scanner = new Scanner(System.in);
+       
         System.out.print("\nEnter the Course ID you want to select: ");
         String selectedCourseId = scanner.nextLine();
 
